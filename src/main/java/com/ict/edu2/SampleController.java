b329639d902dd23972b19f3d698d609a41972704
cho <cho0391@naver.com>
2024-11-15 14:47:44 +0900

    두번째 올리기

--- src/main/java/com/ict/edu2/SampleController.java
+++ src/main/java/com/ict/edu2/SampleController.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -96,4 +99,15 @@ public class SampleController {
 
         // return dataVO;
     }
+
+    @PostMapping("/hi")
+    public String getHi() {
+        return "hi, 안녕하세요, 방가방가";
+    }
+
+    @PostMapping("/hi2")
+    public String[] getHi2(String idx) {
+        return new String[] {"Hello", "World"};
+    }
+    
 }
